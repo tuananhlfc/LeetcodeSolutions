0b20acb578de9603db2ce9a96ae4b7b734007683
Quan <quant1k26cht@gmail.com>
2020-11-26 22:27:37 +0700

    Finish algorithm by recursion

--- fibonaci/Fibonacci.hpp
+++ fibonaci/Fibonacci.hpp
@@ -1,9 +1,16 @@
 #include <cstdint>
+using namespace std;
 
 class Solution {
 public:
     uint64_t fibonacci(const int number) {
         // TODO: Your code here
-        return 0;
+        if(number == 0) {
+            return 0;
+        } else if(number == 1) {
+            return 1;
+        } else {
+            return (fibonacci(number-2) + fibonacci(number-1));        
+        }
     }
 };
